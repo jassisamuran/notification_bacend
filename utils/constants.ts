072557e56538f11ab299c865edcc844abff716f9
jassisamuran <85451341+jassisamuran@users.noreply.github.com>
2025-05-19 13:15:35 +0530

    scripts for delete,adding and getuserId for users (#7)

---
+++ utils/constants.ts
@@ -0,0 +1,5 @@
+export const CONNECTED_TO_MONGODB = "Connected to MongoDB";
+export const MONGODB_URI_NOT_DEFINED = "MONGODB_URI environment variable is not defined.";
+export const MONGODB_CONNECTION_ERROR = "MongoDB connection error:";
+export const MONGODB_DISCONNECTED = "MongoDB disconnected";
+export const MONGODB_CONNECTION_SUCCESS = "MongoDB connected";
