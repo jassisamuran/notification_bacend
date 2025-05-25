a40db4da12cdc1ef0679b75aee0ed4f2a0021c2d
jassisamuran <samuran3132@gmail.com>
2025-05-26 03:10:41 +0530

    add emial provider for  sending emails

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,13 @@
 export const CONNECTED_TO_MONGODB = "Connected to MongoDB";
-export const MONGODB_URI_NOT_DEFINED = "MONGODB_URI environment variable is not defined.";
+export const MONGODB_URI_NOT_DEFINED =
+  "MONGODB_URI environment variable is not defined.";
 export const MONGODB_CONNECTION_ERROR = "MongoDB connection error:";
 export const MONGODB_DISCONNECTED = "MongoDB disconnected";
 export const MONGODB_CONNECTION_SUCCESS = "MongoDB connected";
+
+export const FAILED_EMAIL_LOG = (to: string, error: any) =>
+  `Failed to send email to ${to} : ${error}`;
+
+export const SUCCESS_EMAIL_LOG = (to: string, id: string) => {
+  `Email send to  ${to}, message id ${id}`;
+};
