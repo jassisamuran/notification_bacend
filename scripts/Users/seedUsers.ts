e974d1366f5ff236e45f01b0349b8a2ea490a123
jassisamuran <samuran3132@gmail.com>
2026-01-24 00:03:55 +0530

    added README.md file for project understanding

--- scripts/Users/seedUsers.ts
+++ scripts/Users/seedUsers.ts
@@ -1,49 +1,50 @@
-import mongoose from "mongoose";
 import { faker } from "@faker-js/faker";
 import dotenv from "dotenv";
-dotenv.config();
-import {CONNECTED_TO_MONGODB,MONGODB_DISCONNECTED,MONGODB_URI_NOT_DEFINED} from  '../../utils/constants';
+import mongoose from "mongoose";
 import User from "../../models/User";
+import {
+  CONNECTED_TO_MONGODB,
+  MONGODB_DISCONNECTED,
+  MONGODB_URI_NOT_DEFINED,
+} from "../../utils/constants";
+dotenv.config();
 
-const MONDODB_URI = process.env.MONGODB_URI;
+const MONDODB_URI = "mongodb://localhost:27017";
 if (!MONDODB_URI) {
-    throw new Error(MONGODB_URI_NOT_DEFINED);
+  throw new Error(MONGODB_URI_NOT_DEFINED);
 }
-type UserType={
-    name:String,
-    email:String,
-    phone:String, 
-    isActive:Boolean,
-}
+type UserType = {
+  name: String;
+  email: String;
+  phone: String;
+  isActive: Boolean;
+};
 
-async function seedUsers(count:number){
-    try{
-        await mongoose.connect(MONDODB_URI || "");
-        console.log(CONNECTED_TO_MONGODB);
-        const users:UserType[]=[];
+async function seedUsers(count: number) {
+  try {
+    await mongoose.connect(MONDODB_URI || "");
+    console.log(CONNECTED_TO_MONGODB);
+    const users: UserType[] = [];
 
-        for(let i=0; i<count; i++){
-            users.push({
-                name:faker.person.fullName(),
-                email:faker.internet.email(),
-                phone:`+91${faker.string.numeric(10)}`,
-                isActive:true,
-
-            });
-            console.log(`User ${i+1}:`, users[i]);
-        }
-        await User.insertMany(users);
-        console.log(`${count} users seeded successfully`);
-        process.exit(0);
-
-    }catch(error){
-        console.error("Error seeding users:", error);
-        process.exit(1);
-    }finally{
-        await mongoose.disconnect();
-        console.log(MONGODB_DISCONNECTED);
+    for (let i = 0; i < count; i++) {
+      users.push({
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        phone: `+91${faker.string.numeric(10)}`,
+        isActive: true,
+      });
+      console.log(`User ${i + 1}:`, users[i]);
     }
-
+    await User.insertMany(users);
+    console.log(`${count} users seeded successfully`);
+    process.exit(0);
+  } catch (error) {
+    console.error("Error seeding users:", error);
+    process.exit(1);
+  } finally {
+    await mongoose.disconnect();
+    console.log(MONGODB_DISCONNECTED);
+  }
 }
-const userCount = parseInt(process.argv[2]) || 10; 
+const userCount = parseInt(process.argv[2]) || 10;
 seedUsers(userCount);
